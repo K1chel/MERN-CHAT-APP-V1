cb4578ae5d4be6ce410fcbe1c579a6f5c8ebcb70
Your Name <cherevko022@gmail.com>
2024-04-08 15:10:11 -0400

    chat bubble ui changed

--- client/src/components/ChatHeader.tsx
+++ client/src/components/ChatHeader.tsx
@@ -1,6 +1,7 @@
 import { useSocketContext } from "@/context/SocketContext";
 import { useConversationStore } from "@/store/useConversationStore";
 import { MobileSidebar } from "./MobileSidebar";
+import { UserAvatar } from "./UserAvatar";
 
 export const ChatHeader = () => {
   const { selectedConversation } = useConversationStore();
@@ -13,7 +14,7 @@ export const ChatHeader = () => {
   if (!selectedConversation) return null;
 
   return (
-    <div className="h-16 w-full border-b bg-secondary">
+    <div className="h-[68px] w-full border-b bg-secondary/40">
       <div className="flex items-center justify-between h-full px-6 lg:px-4">
         {/* MOBILE SIDEBAR */}
         <div className="block lg:hidden">
@@ -22,15 +23,26 @@ export const ChatHeader = () => {
         <div />
         <div className="flex items-center gap-x-2">
           <>
-            <h4>
-              Chat with{" "}
-              <span className="font-bold">{selectedConversation.username}</span>
-            </h4>
-            {isOnline ? (
-              <div className="h-2 w-2 rounded-full bg-green-500" />
-            ) : (
-              <div className="h-2 w-2 rounded-full dakr:bg-zinc-500 bg-zinc-400" />
-            )}
+            <UserAvatar
+              src={selectedConversation.avatar}
+              username={selectedConversation.username}
+              className="w-10 h-10"
+            />
+            <div className="flex flex-col">
+              <h6 className="text-sm">
+                Chat with{" "}
+                <span className="font-bold">
+                  {selectedConversation.username}
+                </span>
+              </h6>
+              <span className="ml-auto text-xs text-muted-foreground">
+                {isOnline ? (
+                  <p className="text-green-500">Online</p>
+                ) : (
+                  <p>Offline</p>
+                )}
+              </span>
+            </div>
           </>
         </div>
       </div>
--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -10,69 +10,47 @@ type Props = {
 };
 
 export const Message = ({ message }: Props) => {
+  const { senderId, message: conversationMessage, imageId } = message;
   const { selectedConversation } = useConversationStore();
   const { user } = useContext(AuthContext);
 
-  const fromMe = message.senderId === user?._id;
+  const fromMe = senderId === user?._id;
   const avatarSrc = fromMe ? user?.avatar : selectedConversation?.avatar;
   const username = fromMe ? user?.username : selectedConversation?.username;
 
   return (
-    <>
-      {fromMe ? (
-        <div className="flex justify-end relative">
-          <div className="max-w-96 bg-indigo-500 text-white rounded-t-lg rounded-b-xl gap-3 border whitespace-pre-wrap word-break break-all">
-            {message.message && <p className="px-2 pt-1">{message.message}</p>}
-            {message.imageId && (
-              <div
-                className={cn(
-                  "rounded bg-background/20",
-                  message.message && "mt-2"
-                )}
-              >
-                {/* TODO: in future open modal to see current image */}
-                <img
-                  src={message.imageId}
-                  alt={"Message image"}
-                  className={cn(
-                    "w-50 h-50 rounded object-cover",
-                    message.message && "rounded-t-none"
-                  )}
-                />
-              </div>
-            )}
-            {/* TODO: ADD TIMESTAMPTS */}
+    <div className={cn("flex relative", fromMe && "justify-end")}>
+      <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
+        {!fromMe ? <UserAvatar src={avatarSrc} username={username} /> : null}
+      </div>
+      <div
+        className={cn(
+          // TODO: change rounded for fromMe and from other
+          "max-w-96 rounded-md gap-3 border whitespace-pre-wrap word-break break-all",
+          fromMe
+            ? "bg-indigo-500 text-white rounded-tr-[3px]"
+            : "dark:bg-neutral-600 bg-zinc-200 rounded-tl-[3px]"
+        )}
+      >
+        {conversationMessage && (
+          <p className="px-2 pt-1">{conversationMessage}</p>
+        )}
+        {imageId && (
+          <div className={cn("")}>
+            <img
+              src={imageId}
+              alt={"Message image"}
+              className={cn(
+                "w-50 h-50 rounded object-cover",
+                conversationMessage && "rounded-t-none"
+              )}
+            />
           </div>
-          <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
-            <UserAvatar src={avatarSrc} username={username} />
-          </div>
-        </div>
-      ) : (
-        <div className="flex mb-4">
-          <div className="w-9 h-9 rounded-full flex items-center justify-center mr-2">
-            <UserAvatar src={avatarSrc} username={username} />
-          </div>
-          <div className="max-w-96 dark:bg-white bg-zinc-100 text-white rounded-t-lg rounded-b-xl p-3 gap-3 border whitespace-pre-wrap word-break break-all">
-            <p className="text-gray-700">{message.message}</p>
-            {message.imageId && (
-              <div
-                className={cn(
-                  "rounded bg-background/20",
-                  message.message && "mt-2"
-                )}
-              >
-                {/* TODO: in future open modal to see current image */}
-                <img
-                  src={message.imageId}
-                  alt={"Message image"}
-                  className="w-50 h-50 object-cover rounded"
-                />
-              </div>
-            )}
-            {/* TODO: ADD TIMESTAMPTS */}
-          </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+      <div className="w-9 h-9 rounded-full flex items-center justify-center ml-2">
+        {fromMe ? <UserAvatar src={avatarSrc} username={username} /> : null}
+      </div>
+    </div>
   );
 };
